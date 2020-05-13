e89dcd698bc7e2acce589ef90aeb3fecc8b231e9
Filipe Pereira Rossell <liperossell@gmail.com>
2020-05-12 23:59:15 -0300

    Adicionado comentários na classe UI

--- src/application/UI.java
+++ src/application/UI.java
@@ -2,8 +2,17 @@ package application;
 
 import chess.ChessPiece;
 
+/**
+ * User Interface
+ * @author Filipe
+ *
+ */
 public class UI {
 
+	/**
+	 * Imprime o tabuleiro em branco
+	 * @param Matriz contendo pecas de xadrez
+	 */
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(8 - i + " ");
@@ -15,6 +24,10 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 	}
 	
+	/**
+	 * Metodo auxiliar para imprimir uma peca ou um espaco em branco (-)
+	 * @param Peca para imprimir
+	 */
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
 			System.out.print("-");
